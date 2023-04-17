6161a856748b116eea83ecc999495f0af1f003a3
Lautaro Losio <lautarolosio@gmail.com>
2023-04-17 09:58:18 -0300

    requested changes

--- pokedex/database/get.py
+++ pokedex/database/get.py
@@ -2,37 +2,10 @@
 
 import os
 import requests
-import zlib
 import gzip
 from progressbar import ProgressBar
 
 from .. import resource_path
-
-
-# def download_database():
-#     target = os.path.join(resource_path, "veekun-pokedex.sqlite")
-#     url = "http://veekun.com/static/pokedex/downloads/veekun-pokedex.sqlite.gz"
-    
-#     if os.path.isfile(target):
-#         return
-
-#     request = requests.get(url, stream=True)
-#     total_length = int(request.headers.get("content-length"))
-#     bytes_done = 0
-#     gzipped = ""
-
-#     print("Downloading Veekun Pokédex database...")
-#     with ProgressBar(max_value=total_length) as bar:
-#         for chunk in request.iter_content(chunk_size=1024):
-#             if chunk:
-#                 gzipped += chunk
-#                 bytes_done += len(chunk)
-#                 bar.update(bytes_done)
-
-#     decompressed_data = zlib.decompress(gzipped, 16+zlib.MAX_WBITS)
-
-#     with open(target, "wb") as file:
-#         file.write(decompressed_data)
 
 def download_database():
     target = os.path.join(resource_path, "veekun-pokedex.sqlite")
